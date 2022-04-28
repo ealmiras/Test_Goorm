8d8e83a20d155f42fca4b04e810d88faa55bfa8d
ealmiras <ealmirasozlu@gmail.com>
2022-04-28 17:23:57 +0000

    ..

--- test_goorm.sql
+++ test_goorm.sql
@@ -1,5 +1,10 @@
-# CREATE DATABASE test_goorm;
-# USE test_goorm;
+CREATE DATABASE test_goorm;
+USE test_goorm;
+
 CREATE TABLE test_table (
     name VARCHAR(20),
     age INT);
+
+SHOW tables;
+
+DESC test_table;
